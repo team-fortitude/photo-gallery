833c6f4f450219600faeef4645f70b332b805ced
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-20 15:54:26 -0700

    Used headerTemplate to generate header in index.js

--- src/index/index.js
+++ src/index/index.js
@@ -1,8 +1,13 @@
 import images from '../../assets/data/data.js';
 import imageTemplate from './image-template.js';
 import htmlToDOM from './html-to-DOM.js';
+import headerTemplate from '../header-template.js';
 
 const imagesEl = document.getElementById('images');
+
+const header = headerTemplate();
+const headerDOM = htmlToDOM(header);
+document.body.before(headerDOM);
 
 images.forEach(image => {
     const html = imageTemplate(image);
